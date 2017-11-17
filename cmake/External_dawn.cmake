01820ee6e7eac78e030094494948df57c69f8463
carlos osuna <carlos.osuna@meteoswiss.ch>
2017-11-18 00:30:12 +0100

    add a git download option

--- cmake/External_dawn.cmake
+++ cmake/External_dawn.cmake
@@ -22,13 +22,13 @@ set(DIR_OF_PROTO_EXTERNAL ${CMAKE_CURRENT_LIST_DIR})
 
 function(msbuild_external_package)
   set(options)
-  set(one_value_args URL URL_MD5 DOWNLOAD_DIR SOURCE_DIR)
+  set(one_value_args URL URL_MD5 DOWNLOAD_DIR SOURCE_DIR GIT_REPOSITORY GIT_TAG)
   set(multi_value_args REQUIRED_VARS CMAKE_ARGS)
   cmake_parse_arguments(ARG "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
 
-  msbuild_require_only_one_of(NAME1 "URL" NAME2 "SOURCE_DIR" 
-         GROUP1 ${ARG_URL} ${ARG_DOWNLOAD_DIR} ${ARG_URL_MD5}
-         GROUP2 ${ARG_SOURCE_DIR})
+  msbuild_require_only_one_of2(NAME1 "SOURCE_DIR" NAME2 "GIT"
+         GROUP1 ${ARG_SOURCE_DIR}
+         GROUP2 ${ARG_GIT_REPOSITORY} ${ARG_GIT_TAG})
 
   if(NOT("${ARG_UNPARSED_ARGUMENTS}" STREQUAL ""))
     message(FATAL_ERROR "invalid argument ${ARG_UNPARSED_ARGUMENTS}")
@@ -39,12 +39,13 @@ function(msbuild_external_package)
     SOURCE_DIR source_dir)
 
   # C++ protobuf
-  if(ARG_URL)
+  if(ARG_GIT_REPOSITORY)
     ExternalProject_Add(dawn
       DOWNLOAD_DIR ${ARG_DOWNLOAD_DIR}
-      URL ${ARG_URL}
-      URL_MD5 ${ARG_URL_MD5}
+      GIT_REPOSITORY ${ARG_GIT_REPOSITORY}
+      GIT_TAG ${ARG_GIT_TAG}
       SOURCE_DIR "${source_dir}"
+      SOURCE_SUBDIR "bundle"
       INSTALL_DIR "${install_dir}"
       CMAKE_ARGS ${ARG_CMAKE_ARGS}
     )
