7fe74a26395c14faeb5a74d045a995859552dcd0
carlos osuna <carlos.osuna@meteoswiss.ch>
2017-11-24 21:47:07 +0100

    add

---
+++ cmake/msbuildExportOptions.cmake
@@ -0,0 +1,39 @@
+##===------------------------------------------------------------------------------*- CMake -*-===##
+##                          _                      
+##                         | |                     
+##                       __| | __ ___      ___ ___  
+##                      / _` |/ _` \ \ /\ / / '_  | 
+##                     | (_| | (_| |\ V  V /| | | |
+##                      \__,_|\__,_| \_/\_/ |_| |_| - Compiler Toolchain
+##
+##
+##  This file is distributed under the MIT License (MIT). 
+##  See LICENSE.txt for details.
+##
+##===------------------------------------------------------------------------------------------===##
+
+include(msbuildIncludeGuard)
+msbuild_include_guard()
+
+#.rst:
+# msbuild_export_options
+# -------------------
+#
+# Export a list of options in the variable ``<NAME>_OPTIONS`` which can be accessed by meta
+# projects.
+# 
+# .. code-block:: cmake
+#
+#   msbuild_export_options(NAME ARGN)
+#
+# ``NAME``
+#   Prefix of the options list.
+# ``ARGN``
+#   List of option names to export.
+#
+macro(msbuild_export_options NAME)
+  set("${NAME}_OPTIONS" "" CACHE INTERNAL "Options of ${NAME}" FORCE)
+  foreach(arg ${ARGN})
+    list(APPEND "${NAME}_OPTIONS" "${arg}")
+  endforeach()
+endmacro()
